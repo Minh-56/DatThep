e91a514de7468e970c7fcce63fd5947ecd00c5ee
Tran Thanh Minh <tranthanhminhh55@gmail.com>
2025-06-28 01:34:09 +0700

    thêm các script manager

---
+++ Assets/Script/Manager/SceneController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneController : MonoBehaviour
+{
+    public string mainMenuSceneName = "MainMenu";
+    public string mapSceneName = "MapScene";
+    public string gameOverSceneName = "GameOver";
+    public string battleScenePrefix = "Battle";
+
+    public void LoadMainMenu()
+    {
+        SceneManager.LoadScene(mainMenuSceneName);
+    }
+
+    public void LoadMap()
+    {
+        SceneManager.LoadScene(mapSceneName);
+    }
+
+    public void LoadBattle(int levelIndex)
+    {
+        SceneManager.LoadScene(battleScenePrefix);
+    }
+
+    public void LoadGameOver()
+    {
+        SceneManager.LoadScene(gameOverSceneName);
+    }
+}
